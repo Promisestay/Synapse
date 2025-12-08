557f32f02e7f0191149592846cbe68b518c0f28a
promise <oluwalademorolake@gmail.com>
2025-12-08 16:35:12 +0100

    initial

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -92,7 +92,9 @@ export default function Landing() {
       <footer className="bg-slate-950 py-12 border-t border-slate-800">
         <div className="container max-w-7xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center gap-8">
           <div className="flex items-center gap-2">
-            <div className="w-8 h-8 rounded-lg bg-indigo-600 flex items-center justify-center text-white font-bold text-lg">S</div>
+            <div className="">
+              <img src="./logo2.png" alt="" className="w-20"/>
+            </div>
             <span className="text-xl font-bold text-white tracking-tight">Synapse</span>
           </div>
 
