c4958144e0fbc080140109cd81cb3fd4de007ff0
promise <oluwalademorolake@gmail.com>
2025-12-10 20:37:45 +0100

    initial

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { useNavigate, Link } from "react-router-dom"
-import { Layers, Zap, User, Lock, Mail, ArrowRight, BookOpen, AlertCircle } from "lucide-react"
+import { Layers, Zap, User, Lock, Mail, ArrowRight, BookOpen, AlertCircle, Loader } from "lucide-react"
 import { useAuthStore } from "../store/useAuthStore"
 
 
